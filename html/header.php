ce85c5e01b3e1048f02f50a095488a047aeabbcc
Tarun-Methwani <45097468+Tarun-Methwani@users.noreply.github.com>
2019-04-24 20:23:25 +0530

    first commit

---
+++ html/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="../css/home.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+</head>
+ <body>
+
+
+<header>
+	<div id="topmost">
+		<span id="logo">JS</span>
+		<span id="heading">Jansanjeevani.com</span>
+			<a href="#"><i class="fas fa-phone"></i></a>   |
+			<a href="..//html/cart.html"><i class="fas fa-shopping-cart"></i></a>   |
+			<a href="../html/login.html"><i class="fas fa-user-plus"></i></a> |
+	</div>
+
+	<!--NAVBAR-->
+	<div id="main">
+		<div class="container">
+		<div class="row">
+	        <div class="col-sm-6 col-sm-offset-3">
+	            <div id="imaginary_container"> 
+	                <div class="input-group stylish-input-group">
+	                    <input type="text" class="form-control" placeholder="Search prescription medicines..." >
+	                    <span class="input-group-addon">
+	                        <button type="submit">
+	                            <span class="glyphicon glyphicon-search"></span>
+	                        </button>  
+	                    </span>
+	                </div>
+	            </div>
+	        </div>
+		</div>
+	</div>
+
+		<ul>
+				<li class="active"><a href="../html/homepage.html">Home</a></li>
+		      <li><a href="#">Medicine</a></li>
+		      <li><a href="#">Personal care</a></li>
+		      <li><a href="#">Lifestyle</a></li>
+		      <li><a href="#">Ayurvedic</a></li>
+		      <li><a href="#">Fitness</a></li>
+		      <li><a href="#">Devices</a></li>
+		</ul>
+	</div>
+</header>
